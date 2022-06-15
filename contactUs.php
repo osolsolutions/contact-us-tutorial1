4274f4d1b69f85800bf19f0cb3457d7336b2c059
osolsolutions <osolsolutions@gmail.com>
2022-06-15 17:44:06 +0530

    First commit

---
+++ contactUs.php
@@ -0,0 +1,7 @@
+<?php 
+//validate POST vars 
+require_once('contactUsValidate.php');
+require '../../vendor/autoload.php'; // include vendor/autoload.php to load PHPMailer Class
+require_once('contactUsConfig.php');
+require_once('contactUsExecute.php');
+?>
